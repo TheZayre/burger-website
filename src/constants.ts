69d4a6104f8fc0d9a25764e678adeb3643fa5cec
Jose Angel <thezayrex@gmail.com>
2025-04-01 17:59:37 +0200

    Add Experience component and remove Delivery component

---
+++ src/constants.ts
@@ -0,0 +1,19 @@
+export const experiences = {
+    left: [{
+        company: "Inetum",
+        period: "2024-2022",
+        description: "Migration process, maintenance and development of a web site for the management of customer shipments.",
+        height: "top-1/3",
+    }],
+    right: [{
+        company: "CTT Express",
+        period: "2024-Actually",
+        description: "Migration process, maintenance and development of a web site for the management of customer shipments.",
+        height: "top-2/3",
+    }, {
+        company: "CTT Express",
+        period: "2024-Actually",
+        description: "Migration process, maintenance and development of a web site for the management of customer shipments.",
+        height: "top-3/3",
+    }]
+}
